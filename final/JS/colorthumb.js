a99ed42d053765dfffefe6f103a0fe997cf093fc
jay b <jsborrelli@massart.edu>
2013-12-02 17:03:01 -0500

    MARTHA BARRY JAVA SCRIPT EXTRODINARE

---
+++ final/JS/colorthumb.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+$("#BLACK AND WHITE THUMB YOU NEED THE HASHTAG DOE").mouseover(function () {
+  $("#THIS IS THE DUOTONE IMAGE ALSO HASHTAG DOE").show();document.getElementById('style').style.backgroundColor = "red";
+});
+
+$("#thumb-amsterdam-color").mouseout(function () {
+  $("#thumb-amsterdam-color").hide();document.getElementById('style').style.backgroundColor = "white";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ final/colorthumb.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+$("#carouselposterthumb").mouseover(function () {
+  $("#carouselposterthumbpink").show();document.getElementById('carouselposter').style.backgroundColor = "magenta";
+});
+
+$("#carouselposterthumbpink").mouseout(function () {
+  $("#carouselposterthumbpink").hide();document.getElementById('style').style.backgroundColor = "red";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+});
