2ccf840f3e23aac69a6dce815eb6f8657a0f2d5b
jay b <jsborrelli@massart.edu>
2013-12-02 17:32:01 -0500

    SO CLOSE I CAN TASTE IT

--- final/colorthumb.js
+++ final/colorthumb.js
@@ -1,10 +1,11 @@
 $( document ).ready(function() {
+
 $("#carouselposterthumb").mouseover(function () {
-  $("#carouselposterthumbpink").show();document.getElementById('carouselposter').style.backgroundColor = "magenta";
+  $("#carouselposterthumbpink").show();document.getElementById('carouselposter').style.backgroundColor = "#EC008C";
 });
 
 $("#carouselposterthumbpink").mouseout(function () {
-  $("#carouselposterthumbpink").hide();document.getElementById('style').style.backgroundColor = "red";
+  $("#carouselposterthumbpink").hide();document.getElementById('carouselposter').style.backgroundColor = "#D8D8D8";
 });
 
 
@@ -13,6 +14,64 @@ $("#carouselposterthumbpink").mouseout(function () {
 
 
 
+$("#builtwordthumb").mouseover(function () {
+  $("#builtwordthumbpink").show();document.getElementById('builtword').style.backgroundColor = "#EC008C";
+});
+
+$("#builtwordthumbpink").mouseout(function () {
+  $("#builtwordthumbpink").hide();document.getElementById('builtword').style.backgroundColor = "#D8D8D8";
+});
+
+
+
+
+
+
+$("#sneakersthumb").mouseover(function () {
+  $("#sneakersthumbpink").show();document.getElementById('100sneaks').style.backgroundColor = "#EC008C";
+});
+
+$("#sneakersthumbpink").mouseout(function () {
+  $("#sneakersthumbpink").hide();document.getElementById('100sneaks').style.backgroundColor = "#D8D8D8";
+});
+
+
+
+
+
+
+$("#onsuccessthumb").mouseover(function () {
+  $("#onsuccessthumbpink").show();document.getElementById('onsuccess').style.backgroundColor = "#EC008C";
+});
+
+$("#onsuccessthumbpink").mouseout(function () {
+  $("#onsuccessthumbpink").hide();document.getElementById('onsuccess').style.backgroundColor = "#D8D8D8";
+});
+
+
+
+
+
+
+$("#elementsthumb").mouseover(function () {
+  $("#elementsthumbpink").show();document.getElementById('style').style.backgroundColor = "#EC008C";
+});
+
+$("#elementsthumbpink").mouseout(function () {
+  $("#elementsthumbpink").hide();document.getElementById('style').style.backgroundColor = "#D8D8D8";
+});
+
+
+
+
+
+$("#firstthingsthumb").mouseover(function () {
+  $("#firstthingsthumbpink").show();document.getElementById('firstthingsfirst').style.backgroundColor = "#EC008C";
+});
+
+$("#firstthingsthumbpink").mouseout(function () {
+  $("#firstthingsthumbpink").hide();document.getElementById('firstthingsfirst').style.backgroundColor = "#D8D8D8";
+});
 
 
 
