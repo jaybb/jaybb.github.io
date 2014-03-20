ae92f5b704eb1d77c64b7ca46158ccd08178bae0
jay b <jsborrelli@massart.edu>
2014-03-20 09:21:45 -0400

    test on phone

--- Cart/cart.js
+++ Cart/cart.js
@@ -1,14 +1,18 @@
-  function showItem(name, quantity, price) {
-    var item = $("<div class='item'>");
-    
-    item.append( $("<div class='name'>").text(name) );
-    item.append( $("<div class='price'>").text(quantity + " X $" + price + "=") );
-    item.append( $("<div class='subtotal''>").text("$" + (quantity * price)) );    
-    
-    $(".cart").append(item);
-  }
-  
-  showItem("Shirt",5,20);
-  showItem("Shoes",2,10);
-  showItem("Pants",2,40);
+$(function() {
 
+
+
+
+
+  $(".add").click(function() {
+    var item = $(this).data("item");
+    alert("Added a " + item); 
+  });
+
+
+
+
+
+
+
+});
