75957f6f4db55a2778572f2d4b684bc399a9d796
jay b <jsborrelli@massart.edu>
2014-04-03 00:29:31 -0400

    Can't Get Shit To Center

---
+++ singlescrollin/sneakers.js
@@ -0,0 +1,48 @@
+var Sneakers = {
+
+  hurrache: {
+    title: "Nike Hurrache Trainer Slate Blue",
+    price: 100.00,
+    image: "images/hurrache.jpg",
+    description:  "Released in 2013 the Hurrache Trainer Slate Blue was an instant classic. After selling out immediately from Size? retailers, Nike Silently restocked them after a couple of weeks" ,
+  },
+
+  gelLyte: {
+    title: "Asics Gel Lyte III Volcano",
+    price: 320.00,
+    image: "images/gelLyte.jpg",
+    description:  "Ronnie Fieg hits us with another instant classic ASICS collabo, this Gel Lyte V features a premium red upper as well as other hot pink accents "
+  },
+
+  cat: {
+    title: "Reebok Pump Fury Bape",
+    price: 450.00,
+    image: "images/pumpfury.jpg",
+    description:  "This 2013 collaboration with BAPE set off the year of the Reebok Pump Fury, dozens of companies are dying to collaborate with Reebok on pairs of these and it's all thanks to this Bape Camo Colorway"
+  },
+
+
+  peapod: {
+    title: "Converse Ka II Cordovian Leather",
+    price: 100.00,
+    image: "images/kaii.jpg",
+    description:  "Since Converse's CONS brand retroed the AeroJams in 2013, they have been stepping up their premium shoe game. Only available at select inline stores including New York and San Francisco, these were nearly impossible to get "
+  },
+
+  walter: {
+    title: "Vans Sk8-Hi CNCPTS",
+    price: 300.00,
+    image: "images/sk8hi.jpg",
+    description:  "Boston based sneaker spot Concepts is known for its collaborations with footwear companies to make some of the most premium sneakers out there, the Vans Sk8 Hi is no exeption to that, beautiful print details are accented with a hit of hot pink to tie the whole sneaker together "
+  },
+
+
+  windup: {
+    title: "Nike Lebron XI King's Pride",
+    price: 275.00,
+    image: "images/lebronxi",
+    description:  "Although Lebron himself seems to prefer the tenth iteration of his sneaker line, Nike dropped the 11th version this year and so far they have been nothing but heat. The Kings Pride was the debut sneaker, and it's subtle posite colorway is accented by an intricate print to show the complexity of Jame's Game"
+  }
+
+};
+
