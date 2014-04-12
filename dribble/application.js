091132a4b43f9ff4869a2b0ecf1d47e0e2e6c3a2
jay b <jsborrelli@massart.edu>
2014-04-11 23:00:17 -0400

    Dribble page styled, can't get on click

--- dribble/application.js
+++ dribble/application.js
@@ -12,12 +12,20 @@ $(function (){
    	 	var shot = data.shots[i] 
 
    	 	var postDiv = $("<div class='post'>");
+
+
    	 	var postImage = $("<img class='postImage'>").attr("src",shot.image_teaser_url)
    	 	postDiv.append(postImage)
+
    	 	var postTitle = $("<div class='postTitle'>").text(shot.title)
    	 	postDiv.append(postTitle)
+   	 	
    	 	var author = $("<div class='author'>").text(shot.player.name)
-   	 	postDiv.append(author);
+   	 	postDiv.append(author)
+
+      var likes = $("<div class='likes'>").text(shot.likes_count + " likes")
+      postDiv.append(likes)
+            ;
 
    	 	// other stuff here
    	 	//"http://d13yacurqjgara.cloudfront.net/users/13774/screenshots/1497598/wired_atlanta_teaser.jpg"
@@ -29,5 +37,41 @@ $(function (){
 
 	}); //closes out the $get json function
 
+function showDetails () {
+  $(".overlay").show();
+  $("details").show();
+    }
+
+
+function hideDetails () {
+  $(".overlay").hide();
+  $(".details").hide();
+};
+
+
+ //$function () {
+ // $(".post").click(function (){
+ //   showDetails();
+ // });
+
+// $(".overlay").click(function(){
+//    hideDetails()
+//  }
+
+
 
 }); //closes out the original anonomyous function 
+
+
+
+
+
+
+//ok ultra basic but that ^ establishes were showing and hiding and below actually makes it happen
+//when the page loads 
+
+
+
+
+
+
